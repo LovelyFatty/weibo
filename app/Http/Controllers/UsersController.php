d089b7663309d036f93ffd642e80cbc4c4d34179
vagrant <vagrant@homestead>
2019-01-02 08:53:33 +0000

    增加CSRF验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -17,4 +17,22 @@ class UsersController extends Controller {
         // 通过compact将$user转化为关联数组，作为第二个参数传递给view，将数据与视图绑定
         return view('users.show', compact('user'));
     }
+
+    /*
+        required 存在性验证
+        min max 长度验证
+        email 格式验证
+        unique:users 唯一性验证(这里针对数据表users)
+        confirmed 密码匹配验证
+
+    */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return;
+    }
 }
